15e2c617411b1cc0a0d29255cdc438496dc2dfc2
wantbot_adm <wantbot_adm>
2023-07-07 01:32:58 -0300

    Jquery

---
+++ script.js
@@ -0,0 +1,37 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
+
+
+$(".sir").hide();
+$(".sir1").hide();
+
+setTimeout(() => {
+    $(".sir").slideDown("slow");
+    $(".sir1").fadeIn("slow");
+    
+}, 600)
+
+
+
+
+
+
+
+
